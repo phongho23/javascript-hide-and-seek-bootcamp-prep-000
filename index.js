817701054cb8b7b1a4bf11f58e36fab5282a9020
Phong Ho <ho.phongt@gmail.com>
2018-04-01 20:45:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var lis = document.querySelectorAll('ul.ranked-list li');
+ 
+for (var i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+}
+}
+
+function deepestChild(){
+ var divGrand = document.getElementById("grand-node").getElementsByTagName('div');
+ 
+return divGrand[divGrand.length - 1];
+
+
+  
+  
+}
+
+//return document.getElementById("grand-node").getElementsByTagName('div')[3]
+
+// want divGrand[3];
+
+//Define a function deepestChild() that pulls out the most deeply nested child from div#grand-node. (Remember, you can iterate over elements and call querySelector() and querySelectorAll() on them. This is challenging to implement correctly, but not beyond your ability!)
